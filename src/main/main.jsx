f0965e61be3454b241ab923ecc3ad58ae39d0172
zerone <zerone.dot5@gmail.com>
2023-08-15 18:36:43 +0900

    main make new

--- src/main/main.jsx
+++ src/main/main.jsx
@@ -1,228 +1,52 @@
 import React, { useState, useEffect } from "react";
 import "./main.css";
 import { Link } from "react-router-dom";
-import changecursorImg from "./img/changecursor.gif";
-import isometricImg from "./img/isometric.gif";
-import menuImg from "./img/menu.gif";
-import neonImg from "./img/neon.gif";
-import rainImg from "./img/rain.gif";
+import img1 from "../img/1.png";
+import img2 from "../img/2.png";
+import img3 from "../img/3.png";
+import img4 from "../img/4.png";
+import img5 from "../img/5.png";
+import img6 from "../img/6.png";
 
 let numberList = [
-  { no: 1, name: "ChangeCursor", img: changecursorImg },
-  { no: 2, name: "MenuAnimation", img: menuImg },
-  { no: 3, name: "NeonLightBtn", img: neonImg },
-  { no: 4, name: "IsometricMenu", img: isometricImg },
-  { no: 5, name: "TextRain", img: rainImg },
-  { no: 6, name: "NeonText", img: rainImg },
-  { no: 7, name: "7", img: rainImg },
+  { no: 1, name: "ChangeCursor", img: img1 },
+  { no: 2, name: "MenuAnimation", img: img2 },
+  { no: 3, name: "NeonLightBtn", img: img3 },
+  { no: 4, name: "IsometricMenu", img: img4 },
+  { no: 5, name: "TextRain", img: img5 },
+  { no: 6, name: "NeonText", img: img6 },
+  { no: 7, name: "7", img: img6 },
+  { no: 8, name: "8", img: img6 },
 ];
-let prevNumber = numberList.slice(3);
-let nextNumber = numberList.slice(0, 2);
-let List = [...prevNumber, ...numberList, ...nextNumber];
 
 function Main(props) {
-  const [count, setCount] = useState(4);
-  const [countTop, setCountTop] = useState(283);
-  const [btn, setBtn] = useState(false);
-  const [EclientY, setEclientY] = useState(null);
-  const [transition, setTransition] = useState(`1s all`);
-  const [listArr, setListArr] = useState(List[count].name.split(""));
-  const [timer, setTimer] = useState(-1);
-  const [titleOpacity, setTitleOpacity] = useState(1);
-  const [titleTransition, setTitleTransition] = useState("0.5s all");
-  const [imgOpacity, setImgOpacity] = useState(1);
-
-  const [exOpacity, setExOpacity] = useState(0);
-
-  const pushHandle = (e) => {
-    setBtn(true);
-    setEclientY(e.clientY);
-  };
-
-  const upHandle = () => {
-    setBtn(false);
-  };
-  const moveHandle = (e) => {
-    if (btn && EclientY > e.clientY) {
-      setBtn(false);
-      setCountTop((prev) => prev - 141.5);
-      setCount((prev) => prev + 1);
-    } else if (btn && EclientY < e.clientY) {
-      setBtn(false);
-      setCountTop((prev) => prev + 141.5);
-      setCount((prev) => prev - 1);
-    }
-  };
-
-  console.log(count);
-
-  useEffect(() => {
-    if (count === 2) {
-      setTimeout(() => {
-        setTransition("none");
-        setCountTop(-424.5);
-        setCount(9);
-      }, 1000);
-      setTimeout(() => {
-        setTransition("1s all");
-      }, 1100);
-    } else if (count === 10) {
-      setTimeout(() => {
-        setTransition("none");
-        setCountTop(424.5);
-        setCount(3);
-      }, 1000);
-      setTimeout(() => {
-        setTransition("1s all");
-      }, 1100);
-    }
-
-    setListArr(List[count].name.split(""));
-
-    setTitleTransition("0s all");
-    setTitleOpacity(0);
-    setImgOpacity(0);
-
-    setTimeout(() => {
-      setTimer(-1);
-      setTitleTransition("0.5s all");
-      setTitleOpacity(1);
-      setImgOpacity(1);
-    }, 1000);
-  }, [count]);
-
-  useEffect(() => {
-    let inTimer = -1;
-    setTimeout(() => {
-      const timers = setInterval(() => {
-        inTimer = inTimer + 1;
-        setTimer(inTimer);
-        if (inTimer === listArr.length) {
-          clearInterval(timers);
-        }
-      }, 100);
-
-      return () => clearInterval(timers);
-    }, 1000);
-  }, [listArr]);
-
-  const exHoverHandle = () => {
-    setImgOpacity(0.2);
-    setExOpacity(1);
-  };
-  const exOutHandle = () => {
-    setImgOpacity(1);
-    setExOpacity(0);
-  };
-
   return (
-    <div className="main-contianer">
+    <div className="main-container">
       <div className="main-box">
-        <div
-          className="exBox"
-          onMouseOver={exHoverHandle}
-          onMouseOut={exOutHandle}
-        >
-          {listArr.map((item, idx) => {
+        <h3>
+          Js <small>&</small> Css <small>&</small> React Design Skill
+        </h3>
+        <ul className="main-ul">
+          {numberList.map((item, idx) => {
             return (
-              <Link key={idx} to={List[count].name.toLowerCase()}>
-                <div className="ex-item">
-                  <img
-                    src={List[count].img}
-                    alt="test"
-                    style={{ opacity: imgOpacity }}
-                  ></img>
-                  <div>
-                    <p
-                      style={{
-                        fontSize: "20px",
-                        opacity: exOpacity,
-                        transition: `1s all`,
-                      }}
-                    >
-                      Viewmore
-                    </p>
+              <Link key={idx} to={`${item.name.toLowerCase()}`}>
+                <li className="main-list">
+                  <div className="main-imgBx">
+                    <img src={item.img} />
                   </div>
-                </div>
+                  <div className="main-content">
+                    <h2 className="rank">
+                      <small>#</small>
+                      {item.no}
+                    </h2>
+                    <h4>{item.name}</h4>
+                    <p> Lorem, ipsum.</p>
+                  </div>
+                </li>
               </Link>
             );
           })}
-        </div>
-        <ul className="listBox">
-          {List.map((item, idx) => {
-            return (
-              <li
-                key={idx}
-                style={
-                  idx === count
-                    ? {
-                        top: `${countTop}px`,
-                        color: "white",
-                        transition: transition,
-                      }
-                    : idx < count
-                    ? {
-                        top: `${countTop}px`,
-                        opacity: 0.7,
-
-                        transition: transition,
-                      }
-                    : idx > count
-                    ? {
-                        top: `${countTop}px`,
-                        opacity: 0.7,
-
-                        transition: transition,
-                      }
-                    : {}
-                }
-                onMouseDown={(e) => pushHandle(e)}
-                onMouseUp={upHandle}
-                onMouseMove={(e) => moveHandle(e)}
-              >
-                <p
-                  style={
-                    idx === count
-                      ? { color: "white", transition: transition }
-                      : idx < count
-                      ? {
-                          transform: `rotateX(70deg)`,
-                          transition: transition,
-                        }
-                      : idx > count
-                      ? {
-                          transform: `rotateX(-70deg)`,
-                          transition: transition,
-                        }
-                      : {}
-                  }
-                >
-                  {item.no}
-                </p>
-              </li>
-            );
-          })}
         </ul>
-
-        <div className="titleBox">
-          {listArr.map((item, idx) => {
-            return (
-              <h2
-                key={idx}
-                style={
-                  idx <= timer
-                    ? {
-                        opacity: titleOpacity,
-                        transition: titleTransition,
-                      }
-                    : { opacity: 0, transition: titleTransition }
-                }
-              >
-                {item}
-              </h2>
-            );
-          })}
-        </div>
       </div>
     </div>
   );
