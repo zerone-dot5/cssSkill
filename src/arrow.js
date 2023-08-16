cbe106a2ae2b220a5759ae5da36d70aff24b5b51
zerone <zerone.dot5@gmail.com>
2023-08-16 18:48:33 +0900

    theme toggle 80%

--- src/arrow.js
+++ src/arrow.js
@@ -1,9 +1,22 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 function Arrow(props) {
+  const [display, setDisplay] = useState(0);
+  const location = useLocation();
+
+  let currentPath = "";
+  useEffect(() => {
+    if (currentPath === location.pathname) window.location.reload();
+    if (location.pathname === "/") {
+      setDisplay(0);
+    } else {
+      setDisplay(1);
+    }
+    console.log(location.pathname);
+  }, [location]);
   return (
-    <div className="arrow-container">
+    <div className="arrow-container" style={{ opacity: display }}>
       <Link to="/" className="arrow-box">
         <ion-icon name="arrow-back-outline"></ion-icon>
       </Link>
--- src/main/main.jsx
+++ src/main/main.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./main.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import img1 from "../img/1.png";
 import img2 from "../img/2.png";
 import img3 from "../img/3.png";
@@ -9,23 +9,58 @@ import img5 from "../img/5.png";
 import img6 from "../img/6.png";
 
 let numberList = [
-  { no: 1, name: "ChangeCursor", img: img1 },
-  { no: 2, name: "MenuAnimation", img: img2 },
-  { no: 3, name: "NeonLightBtn", img: img3 },
-  { no: 4, name: "IsometricMenu", img: img4 },
-  { no: 5, name: "TextRain", img: img5 },
-  { no: 6, name: "NeonText", img: img6 },
-  { no: 7, name: "7", img: img6 },
-  { no: 8, name: "8", img: img6 },
+  {
+    no: 1,
+    name: "ChangeCursor",
+    img: img1,
+    ex: "Change BgColor and FontColor",
+  },
+  { no: 2, name: "MenuAnimation", img: img2, ex: "Menu Toggle Animation" },
+  { no: 3, name: "NeonLightBtn", img: img3, ex: "Neon Light Button" },
+  { no: 4, name: "IsometricMenu", img: img4, ex: "3D Menu" },
+  { no: 5, name: "TextRain", img: img5, ex: "text Rain" },
+  { no: 6, name: "NeonText", img: img6, ex: "soft neon text" },
+  { no: 7, name: "7", img: img6, ex: "not" },
+  { no: 8, name: "8", img: img6, ex: "not" },
 ];
 
+let WhiteTheme = {
+  bgColor: "#f5f5f5",
+  listBoxColor: "#fff",
+};
+
+let BlackTheme = {
+  bgColor: "#333",
+  listBoxColor: "#000",
+};
+
 function Main(props) {
+  const [toggleClass, setToggleClass] = useState("");
+  const [toggleColor, setToggleColor] = useState(WhiteTheme);
+
+  const ToggleHandle = () => {
+    toggleClass === "" ? setToggleClass("black") : setToggleClass("");
+    toggleColor === WhiteTheme
+      ? setToggleColor(BlackTheme)
+      : setToggleColor(WhiteTheme);
+  };
+
+  console.log(toggleColor.bgColor);
   return (
-    <div className="main-container">
+    <div className="main-container" style={{ background: toggleColor.bgColor }}>
       <div className="main-box">
         <h3>
           Js <small>&</small> Css <small>&</small> React Design Skill
         </h3>
+        <div className="main-themebox">
+          <button
+            id="main-theme"
+            className={toggleClass}
+            onClick={ToggleHandle}
+          >
+            <div className="main-toggle"></div>
+          </button>
+        </div>
         <ul className="main-ul">
           {numberList.map((item, idx) => {
             return (
@@ -40,7 +75,7 @@ function Main(props) {
                       {item.no}
                     </h2>
                     <h4>{item.name}</h4>
-                    <p> Lorem, ipsum.</p>
+                    <p>{item.ex}</p>
                   </div>
                 </li>
               </Link>
